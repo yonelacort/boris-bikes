10ce400967b90c7208d4187aeb4e44c384cae031
apostolistax <a.taxidaridis@gmail.com>
2014-04-14 14:09:21 +0100

    Implementing broken? method

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,19 @@
 class Bike
-  def broken?
 
+  # the initialize method is always called when you create a new
+  # class by typing Bike.new
+  def initialize
+    @broken = false
   end
+
+
+
+  def broken?
+    @broken
+  end
+
+  def break
+    @broken = true
+  end
+
 end
